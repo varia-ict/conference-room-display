6440e60f855b38d0d69d675e33d7ec0638884923
RinaKovri <ekaterina.romanna@gmail.com>
2022-09-14 10:49:58 +0300

    test

---
+++ src/components/calendarButton/calendarButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './CalendarButton.module.scss'
+type CalendarButtonProps = {
+
+}
+
+const CalendarButton: React.FC<CalendarButtonProps> = () => {
+  return (
+    <div className={styles.container}>
+      CalendarButton
+    </div>
+  )
+}
+
+export default CalendarButton

94309459711a269ca7a98ea76ec20a03c600ee9c
RinaKovri <ekaterina.romanna@gmail.com>
2022-09-14 11:42:00 +0300

    deleted jonis parts of code from App.tsc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import DemoComponent from './components/demoComponent/demoComponent'
+
 import styles from './app.module.scss';
-import Demo2 from './components/demo2/demo2';
+
+import CalendarButton from './components/calendarButton/calendarButton';
 
 type AppProps = {
 
@@ -10,8 +11,7 @@ type AppProps = {
 const App: React.FC<AppProps> = () => {
   return (
     <div className={styles.container}>
-      <DemoComponent />
-      <Demo2 />
+      <CalendarButton />
     </div>
   )
 }
--- src/components/calendarButton/calendarButton.tsx
+++ src/components/calendarButton/calendarButton.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from './CalendarButton.module.scss'
+import React from 'react';
+import styles from './calendarButton.module.scss';
 type CalendarButtonProps = {
 
 }

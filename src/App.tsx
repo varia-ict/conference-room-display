f40688f568d16bd304ea9cf779a2d96776b60b3f
SsaraM <sara.mustapha26@gmail.com>
2022-09-22 13:55:47 +0300

    app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import styles from './app.module.scss';
-import background from './images/varialogo.png'
 
-type AppProps = {
-
-}
-
-const App: React.FC<AppProps> = () => {
+const App: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.background}>
-        <img src={background}/>
       </div>
       <div className={styles.content}>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <div className={styles.left}>
+          <div className={styles.top}></div>
+          <div className={styles.bottom}></div>
+        </div>
+        <div className={styles.right}></div>
       </div>
     </div>
   )

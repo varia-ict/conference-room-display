d81ce0a416d8db7da224236bbc54cc93b328d75d
RinaKovri <ekaterina.romanna@gmail.com>
2022-09-27 14:59:50 +0300

    added overlaw effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import styles from './app.module.scss';
+import Home from './components/calendarButton/home';
+
+
+
+type AppProps = {
+
+}
 
 const App: React.FC = () => {
   return (
@@ -9,12 +16,18 @@ const App: React.FC = () => {
       <div className={styles.content}>
         <div className={styles.left}>
           <div className={styles.top}></div>
-          <div className={styles.bottom}></div>
+          <div className={styles.bottom}>
+            <Home />
+          </div>
         </div>
         <div className={styles.right}></div>
       </div>
     </div>
   )
 }
+
+
+
+
 
 export default App
---
+++ src/components/calendarButton/calendarButton.tsx
@@ -0,0 +1,34 @@
+import styles from './calendarButton.module.scss';
+import CalendarIcon from './calendar-icon.png';
+
+
+
+type CalendarButtonProps = {
+  // pageName: translations
+  onClick?: () => void
+}
+// interface translations {
+//   english: string
+//   finnish: string
+// }
+
+
+
+//component recieves a start and end date and redirect to calendar component
+export function CalendarButton({ onClick }: CalendarButtonProps) {
+  return (
+    <button className={styles.btn} onClick={onClick}>
+
+      <img className={styles.img}
+        src={CalendarIcon}
+        alt='calendar' />
+    </button>
+  )
+}
+
+
+
+
+
+export default CalendarButton
+

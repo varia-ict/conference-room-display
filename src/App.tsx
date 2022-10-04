7cc4b2d988cf7dfe138380761c2e8990e75830d9
stewpika <hyy5752@163.com>
2022-10-04 12:56:39 +0300

    created new branch for team2

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
-import DemoComponent from './components/demoComponent/demoComponent'
 import styles from './app.module.scss';
 
 const App: React.FC = () => {
   return (
-    // <DemoComponent/>
     <div className={styles.container}>
-      <div className={styles.background}>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.left}>
-          <div className={styles.top}></div>
-          <div className={styles.bottom}></div>
-        </div>
-        <div className={styles.right}></div>
-      </div>
+
     </div>
   )
 }

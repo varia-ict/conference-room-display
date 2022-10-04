dd0de9d7df249afb30cdec333cd113645f29332f
stewpika <hyy5752@163.com>
2022-10-05 02:13:04 +0300

    set up background

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import styles from './app.module.scss';
 const App: React.FC = () => {
   return (
     <div className={styles.container}>
-
+      <div className={styles.background} />
+      <div className={styles.left}>
+        <div className={styles.top}>
+        </div>
+        <div className={styles.bottom}>
+        </div>
+      </div>
+      <div className={styles.right}>
+      </div>
     </div>
   )
 }

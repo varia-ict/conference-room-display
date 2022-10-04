6160b21f3b6f5e623ecc17d3a0013529a7703c6c
abhi1988sharma <abhi1988.sharma@gmail.com>
2022-10-04 13:19:06 +0300

    changed room number

--- src/components/huoneNo/roomNo.tsx
+++ src/components/huoneNo/roomNo.tsx
@@ -8,7 +8,7 @@ type RoomComponentProps = {
 const RoomComponent: React.FC<RoomComponentProps> = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.item}>Luokka 216</div>
+      <div className={styles.item}>Luokka 217</div>
       
     </div>
   )

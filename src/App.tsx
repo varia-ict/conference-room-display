a328cd21ccbf83e67b44c25a40cadd92f9d4c9d7
GolyshMariia <g.mv@mail.ru>
2022-10-05 13:35:53 +0300

    new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import styles from './app.module.scss';
+import Timeline from './components/Reservation/reservation';
 
 const App: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.background}>
       </div>
-      <div className={styles.content}>
-        <div className={styles.left}>
+      <div className={styles.content} id='1'>
+        <div className={styles.left} id='2'>
           <div className={styles.top}></div>
-          <div className={styles.bottom}></div>
+          <div className={styles.bottom}>
+            <Timeline />
+            </div>
         </div>
         <div className={styles.right}></div>
       </div>

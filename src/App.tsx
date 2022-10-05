f90131bc887e1e70581c6cf41c4a9462106c7431
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-10-05 11:28:37 +0300

    main branch for scrum team 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,7 @@ import styles from './app.module.scss';
 
 const App: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.background}>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.left}>
-          <div className={styles.top}></div>
-          <div className={styles.bottom}></div>
-        </div>
-        <div className={styles.right}></div>
-      </div>
-    </div>
+    <div className={styles.container}></div>
   )
 }
 

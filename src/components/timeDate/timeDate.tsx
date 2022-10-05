87fa57fb5412310fdfdef7a1cca713c99fb48caf
mikajuhani <mikajuhani13@gmail.com>
2022-10-05 13:33:33 +0300

    DateTime

---
+++ src/components/timeDate/timeDate.tsx
@@ -0,0 +1,26 @@
+import  { useState , useEffect } from 'react'
+import styles from './timeDate.module.scss';
+
+const DateTime = () => {
+
+  
+  var [date,setDate] = useState(new Date());
+
+  useEffect(() => {
+      var timer = setInterval(()=>setDate(new Date()), 1000 )
+      return function cleanup() {
+          clearInterval(timer)
+      }
+
+  });
+
+  return(
+      <div >
+          <p className={styles.time}>{date.toLocaleTimeString()}</p>
+          <p className={styles.date}>{date.toLocaleDateString()}</p>
+
+      </div>
+  )
+}
+
+export default DateTime
